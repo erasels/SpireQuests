2a379ff396d1f59adccdd0a19b66b692a506a0ab
erasels <emanuelbedburdick@yahoo.de>
2025-11-15 21:35:46 +0100

    feat: Add RagerQuest

---
+++ src/main/java/spireQuests/quests/gk/RagerQuest.java
@@ -0,0 +1,21 @@
+package spireQuests.quests.gk;
+
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.potions.AttackPotion;
+import com.megacrit.cardcrawl.potions.StrengthPotion;
+import spireQuests.patches.QuestTriggers;
+import spireQuests.quests.AbstractQuest;
+import spireQuests.quests.QuestReward;
+
+public class RagerQuest extends AbstractQuest {
+    public RagerQuest() {
+        super(QuestType.SHORT, QuestDifficulty.NORMAL);
+
+        new TriggerTracker<>(QuestTriggers.PLAY_CARD, 5)
+                .triggerCondition((card) -> card.type == AbstractCard.CardType.ATTACK)
+                .add(this);
+
+        addReward(new QuestReward.PotionReward(new AttackPotion()));
+        addReward(new QuestReward.PotionReward(new StrengthPotion()));
+    }
+}
