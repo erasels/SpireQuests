aa0112c61c0369648844edb28a8d6393ede85964
modargo <modargo@gmail.com>
2025-11-16 16:03:07 -0500

    Add Chart the Unknown quest

---
+++ src/main/java/spireQuests/quests/modargo/ChartTheUnknownQuest.java
@@ -0,0 +1,24 @@
+package spireQuests.quests.modargo;
+
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.rooms.EventRoom;
+import spireQuests.patches.QuestTriggers;
+import spireQuests.quests.AbstractQuest;
+import spireQuests.quests.QuestReward;
+
+public class ChartTheUnknownQuest extends AbstractQuest {
+    public ChartTheUnknownQuest() {
+        super(QuestType.SHORT, QuestDifficulty.EASY);
+
+        new TriggerTracker<>(QuestTriggers.ENTER_ROOM, 5)
+                .triggerCondition((node) -> node.room instanceof EventRoom)
+                .add(this);
+
+        addReward(new QuestReward.RelicReward(new OwlMask()));
+    }
+
+    @Override
+    public boolean canSpawn() {
+        return AbstractDungeon.actNum == 1 || AbstractDungeon.actNum == 2;
+    }
+}
---
+++ src/main/java/spireQuests/quests/modargo/OwlMask.java
@@ -0,0 +1,23 @@
+package spireQuests.quests.modargo;
+
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.rooms.AbstractRoom;
+import com.megacrit.cardcrawl.rooms.EventRoom;
+import spireQuests.abstracts.AbstractSQRelic;
+
+import static spireQuests.Anniv8Mod.makeID;
+
+public class OwlMask extends AbstractSQRelic {
+    public static final String ID = makeID(OwlMask.class.getSimpleName());
+    public OwlMask() {
+        super(ID, "modargo", RelicTier.SPECIAL, LandingSound.MAGICAL);
+    }
+
+    @Override
+    public void onEnterRoom(AbstractRoom room) {
+        if (room instanceof EventRoom) {
+            this.flash();
+            AbstractDungeon.player.increaseMaxHp(2, true);
+        }
+    }
+}
