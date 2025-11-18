2222130277ed511b7d6d248e21f6e2a4ada66a58
Mangochicken <mangochkn@gmail.com>
2025-11-18 21:36:03 +0800

    finish quest trigger logic

--- src/main/java/spireQuests/quests/mangochicken/HandicapQuest.java
+++ src/main/java/spireQuests/quests/mangochicken/HandicapQuest.java
@@ -1,5 +1,6 @@
 package spireQuests.quests.mangochicken;
 
+import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch;
 import com.megacrit.cardcrawl.actions.GameActionManager;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.rooms.MonsterRoomElite;
@@ -9,19 +10,16 @@ import spireQuests.quests.AbstractQuest;
 
 
 public class HandicapQuest extends AbstractQuest {
-
-    static int turnNum = 1;
-
     public HandicapQuest() {
         super(QuestType.SHORT, QuestDifficulty.NORMAL);
 
-        // Trigger only on the first turn of an elite combat,
+        // Trigger only on the first turn of an elite combat
         new TriggerTracker<>(QuestTriggers.TURN_END, 1)
-                .triggerCondition((x)-> AbstractDungeon.actionManager.cardsPlayedThisTurn.isEmpty()
-                        && AbstractDungeon.getCurrRoom() instanceof MonsterRoomElite
-                        && turnNum == 1);
-
+            .triggerCondition((x)-> AbstractDungeon.actionManager.cardsPlayedThisTurn.isEmpty()
+                && AbstractDungeon.getCurrRoom() instanceof MonsterRoomElite
+                && GameActionManager.turn == 1)
+            .setResetTrigger(QuestTriggers.PLAY_CARD)
+            .setResetTrigger(QuestTriggers.VICTORY) // maybe add ENTER_ROOM and LEAVE_ROOM just to make sure
+            .add(this);
     }
-
-
 }
