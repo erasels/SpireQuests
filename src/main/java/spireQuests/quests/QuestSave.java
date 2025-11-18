e160b5957729a33f5641c0bdec897ef0eadc128e
SoytheProton <alttimelinemod@gmail.com>
2025-11-17 20:31:55 -0800

    Update QuestSave.java

--- src/main/java/spireQuests/quests/QuestSave.java
+++ src/main/java/spireQuests/quests/QuestSave.java
@@ -17,6 +17,10 @@ public class QuestSave {
         questRewards = new QuestReward.QuestRewardSave[quests.size()][];
 
         for (int i = 0; i < quests.size(); ++i) {
+            if(quests.get(i).questboundCards != null) for(AbstractCard c : quests.get(i).questboundCards) {
+                QuestboundMod mod = (QuestboundMod) CardModifierManager.getModifiers(c, QuestboundMod.ID).get(0);
+                if(mod != null) mod.boundQuestIndex = i;
+            }
             questIds[i] = quests.get(i).id;
             questData[i] = quests.get(i).trackerSaves();
             questRewards[i] = quests.get(i).rewardSaves();
