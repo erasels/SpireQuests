d553d110c85d41cde53dad8b1626180e42e49fae
Indi Esneault <indiesneault@gmail.com>
2025-11-20 17:00:18 -0800

    create Gold Stake quest

---
+++ src/main/java/spireQuests/quests/indi_keurodz/BalatroQuest.java
@@ -0,0 +1,24 @@
+package spireQuests.quests.indi_keurodz;
+
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.relics.BurningBlood;
+
+import spireQuests.patches.QuestTriggers;
+import spireQuests.quests.AbstractQuest;
+import spireQuests.quests.QuestReward;
+
+public class BalatroQuest extends AbstractQuest {
+    public static int BLIND_FIGHTS_COMPLETED = 0;
+
+    public BalatroQuest() {
+        super(QuestType.LONG, QuestDifficulty.CHALLENGE);
+
+        new TriggeredUpdateTracker<>(QuestTriggers.VICTORY, 0, 10, () -> {
+            return BLIND_FIGHTS_COMPLETED;
+        }).add(this);
+
+        addReward(new QuestReward.RelicReward(new BurningBlood()));
+        needHoverTip = true;
+    }
+
+}
