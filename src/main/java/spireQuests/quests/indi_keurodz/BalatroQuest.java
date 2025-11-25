5d3774c5173c8d81b456b14714c99ebf4465a2ea
Indi Esneault <indiesneault@gmail.com>
2025-11-24 19:08:06 -0800

    load boss blind textures in atlas

--- src/main/java/spireQuests/quests/indi_keurodz/BalatroQuest.java
+++ src/main/java/spireQuests/quests/indi_keurodz/BalatroQuest.java
@@ -1,17 +1,25 @@
 package spireQuests.quests.indi_keurodz;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.megacrit.cardcrawl.relics.BurningBlood;
 
+import spireQuests.Anniv8Mod;
 import spireQuests.patches.QuestTriggers;
 import spireQuests.quests.AbstractQuest;
 import spireQuests.quests.QuestReward;
 import spireQuests.quests.indi_keurodz.relics.GoldStakeRelic;
 
 public class BalatroQuest extends AbstractQuest {
+    private static TextureAtlas BossBlinds;
+
     public static int BLIND_FIGHTS_COMPLETED = 0;
 
     public BalatroQuest() {
         super(QuestType.LONG, QuestDifficulty.CHALLENGE);
+
+        BossBlinds = new TextureAtlas(
+                Gdx.files.internal(Anniv8Mod.makeContributionPath("indi_keurodz", "BossBlinds.atlas")));
 
         new TriggeredUpdateTracker<>(QuestTriggers.VICTORY, 0, 10, () -> {
             return BLIND_FIGHTS_COMPLETED;
