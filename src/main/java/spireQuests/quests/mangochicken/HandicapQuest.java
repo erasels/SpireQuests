ab30ddfce689f2f4e945c4ce845fb32cf3c26236
Mangochicken13 <97166753+Mangochicken13@users.noreply.github.com>
2025-11-26 20:13:48 +0800

    fix the everything - Added relic sprite outline - Reduced relic sprite sizes - Fixed localization file strings

--- src/main/java/spireQuests/quests/mangochicken/HandicapQuest.java
+++ src/main/java/spireQuests/quests/mangochicken/HandicapQuest.java
@@ -1,25 +1,32 @@
 package spireQuests.quests.mangochicken;
 
-import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch;
 import com.megacrit.cardcrawl.actions.GameActionManager;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.rooms.MonsterRoomElite;
 import spireQuests.patches.QuestTriggers;
 import spireQuests.quests.AbstractQuest;
-
+import spireQuests.quests.QuestReward;
+import spireQuests.quests.mangochicken.relics.HeartyAnchor;
 
 
 public class HandicapQuest extends AbstractQuest {
     public HandicapQuest() {
         super(QuestType.SHORT, QuestDifficulty.NORMAL);
 
-        // Trigger only on the first turn of an elite combat
+
         new TriggerTracker<>(QuestTriggers.TURN_END, 1)
             .triggerCondition((x)-> AbstractDungeon.actionManager.cardsPlayedThisTurn.isEmpty()
                 && AbstractDungeon.getCurrRoom() instanceof MonsterRoomElite
                 && GameActionManager.turn == 1)
             .setResetTrigger(QuestTriggers.PLAY_CARD)
-            .setResetTrigger(QuestTriggers.VICTORY) // maybe add ENTER_ROOM and LEAVE_ROOM just to make sure
+            .setResetTrigger(QuestTriggers.VICTORY)
             .add(this);
+
+        addReward(new QuestReward.RelicReward(new HeartyAnchor()));
+    }
+
+    @Override
+    public boolean canSpawn() {
+        return AbstractDungeon.actNum == 1;
     }
 }
--- src/main/java/spireQuests/quests/mangochicken/relics/HeartyAnchor.java
+++ src/main/java/spireQuests/quests/mangochicken/relics/HeartyAnchor.java
@@ -1,12 +1,9 @@
 package spireQuests.quests.mangochicken.relics;
 
-import basemod.abstracts.cardbuilder.actionbuilder.ActionBuilder;
 import com.evacipated.cardcrawl.mod.stslib.actions.tempHp.AddTemporaryHPAction;
-import com.megacrit.cardcrawl.actions.GameActionManager;
+import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import com.megacrit.cardcrawl.rooms.AbstractRoom;
 import spireQuests.abstracts.AbstractSQRelic;
-import spireQuests.util.Wiz;
 
 import static spireQuests.Anniv8Mod.makeID;
 
@@ -14,14 +11,14 @@ public class HeartyAnchor extends AbstractSQRelic {
     public static final String ID = makeID(HeartyAnchor.class.getSimpleName());
 
     public HeartyAnchor() {
-        super(ID, "mangochicken", RelicTier.SPECIAL, LandingSound.SOLID);
+        super(ID, "mangochicken", RelicTier.SPECIAL, LandingSound.MAGICAL);
     }
 
     @Override
-    public void atTurnStart() {
-        if (GameActionManager.turn == 1 && Wiz.isInCombat()) {
-            this.flash();
-            addToBot(new AddTemporaryHPAction(AbstractDungeon.player, AbstractDungeon.player, 6));
-        }
+    public void atBattleStart() {
+        this.flash();
+        this.addToBot(new RelicAboveCreatureAction(AbstractDungeon.player, this));
+        this.addToBot(new AddTemporaryHPAction(AbstractDungeon.player, AbstractDungeon.player, 6));
+        this.grayscale = true;
     }
 }
