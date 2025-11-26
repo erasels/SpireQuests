c33dab0791390138362ac57e64dbe4c14e434695
Kevin Rodriguez <77088132+Keurodz@users.noreply.github.com>
2025-11-25 22:47:46 -0500

    Updated stickers to have tooltips

--- src/main/java/spireQuests/quests/indi_keurodz/PerishableStickerModifier.java
+++ src/main/java/spireQuests/quests/indi_keurodz/PerishableStickerModifier.java
@@ -1,20 +1,28 @@
 package spireQuests.quests.indi_keurodz;
 
+import basemod.helpers.TooltipInfo;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.evacipated.cardcrawl.mod.stslib.util.extraicons.ExtraIcons;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 
 import basemod.abstracts.AbstractCardModifier;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.UIStrings;
 import spireQuests.Anniv8Mod;
 import spireQuests.quests.AbstractQuest;
 import spireQuests.quests.indi_keurodz.patches.BattleEndPatch;
 import spireQuests.util.TexLoader;
 
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.List;
+
 public class PerishableStickerModifier extends AbstractCardModifier {
 
     public static String MODIFIER_ID = Anniv8Mod.makeID("PerishableSticker");
+    private static final UIStrings strings = CardCrawlGame.languagePack.getUIString(MODIFIER_ID);
 
     public int REMAINING_TURNS;
 
@@ -39,6 +47,13 @@ public class PerishableStickerModifier extends AbstractCardModifier {
     @Override
     public void onInitialApplication(AbstractCard card) {
         REMAINING_TURNS = 5;
+    }
+
+    @Override
+    public List<TooltipInfo> additionalTooltips(AbstractCard card) {
+        List<TooltipInfo> tips = new ArrayList<>();
+        tips.add(new TooltipInfo(strings.TEXT[0], String.format(strings.TEXT[1], REMAINING_TURNS)));
+        return tips;
     }
 
     /*
