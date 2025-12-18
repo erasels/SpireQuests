ba3f258b2a44f9580e245c43d26c73c879ec71bb
Kevin Rodriguez <77088132+Keurodz@users.noreply.github.com>
2025-12-17 23:05:41 -0500

    Fixed load permanence of Violet Vessel and Wall?

--- src/main/java/spireQuests/quests/indi_keurodz/BossBlinds/VioletVessel.java
+++ src/main/java/spireQuests/quests/indi_keurodz/BossBlinds/VioletVessel.java
@@ -4,18 +4,27 @@ import com.evacipated.cardcrawl.modthespire.lib.SpirePatch2;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.rooms.AbstractRoom;
+import com.megacrit.cardcrawl.rooms.MonsterRoom;
+import com.megacrit.cardcrawl.rooms.MonsterRoomElite;
 import com.megacrit.cardcrawl.vfx.combat.HealEffect;
 import spireQuests.quests.indi_keurodz.patches.ShowBossBlindsOnMapPatch.BossBlindField;
 import spireQuests.quests.indi_keurodz.BalatroQuest.BossBlind;
 
 public class VioletVessel {
 
-    @SpirePatch2(clz = AbstractMonster.class, method = "init")
+    private static boolean hasBuffedEnemies = false;
+
+    @SpirePatch2(clz = AbstractRoom.class, method = "update")
     public static class EnemyHealthBuffPatch {
         @SpirePostfixPatch
-        public static void Postfix(AbstractMonster __instance) {
+        public static void Postfix(AbstractRoom __instance) {
+            if (hasBuffedEnemies) return;
             BossBlind blind = BossBlindField.blind.get(AbstractDungeon.getCurrMapNode());
-            if (blind == null) return;
+            if (blind == null) {
+                hasBuffedEnemies = true;
+                return;
+            }
 
             float hpMod = 0.0f;
             switch (blind) {
@@ -29,16 +38,37 @@ public class VioletVessel {
                     return;
             }
 
-            int extraHP = (int)(__instance.maxHealth * hpMod);
-            __instance.maxHealth += extraHP;
-            __instance.currentHealth += extraHP;
-            __instance.healthBarUpdatedEvent();
+            for (AbstractMonster m : AbstractDungeon.getCurrRoom().monsters.monsters) {
+                if (m != null && !m.isDying && !m.isEscaping) {
+                    int extraHP = (int)(m.maxHealth * hpMod);
+                    m.maxHealth += extraHP;
+                    m.currentHealth += extraHP;
+                    m.healthBarUpdatedEvent();
 
-            AbstractDungeon.effectList.add(new HealEffect(
-                    __instance.hb.cX,
-                    __instance.hb.cY,
-                    extraHP
-            ));
+                    AbstractDungeon.effectList.add(new HealEffect(
+                            m.hb.cX,
+                            m.hb.cY,
+                            extraHP
+                    ));
+                }
+            }
+            hasBuffedEnemies = true;
+        }
+    }
+
+    @SpirePatch2(clz = MonsterRoom.class, method = "onPlayerEntry")
+    public static class ResetFlagPatch {
+        @SpirePostfixPatch
+        public static void Postfix() {
+            hasBuffedEnemies = false;
+        }
+    }
+
+    @SpirePatch2(clz = MonsterRoomElite.class, method = "onPlayerEntry")
+    public static class ResetEliteFlagPatch {
+        @SpirePostfixPatch
+        public static void Postfix() {
+            hasBuffedEnemies = false;
         }
     }
 }
