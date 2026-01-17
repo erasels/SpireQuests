81b98afa18690b33cb955e79a24ee08a771fc46e
Quorry <portrate@gmail.com>
2026-01-17 01:04:16 -0500

    Fatal Strikes Quest

---
+++ src/main/java/spireQuests/quests/quorry/FatalPatch.java
@@ -0,0 +1,25 @@
+package spireQuests.quests.quorry;
+
+import com.evacipated.cardcrawl.mod.stslib.damagemods.DamageModifierManager;
+import com.evacipated.cardcrawl.modthespire.lib.SpireInsertPatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch2;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+
+@SpirePatch2(clz = DamageAction.class, method = "update")
+public class FatalPatch {
+
+    @SpireInsertPatch(
+            loc=93
+    )
+    public static void Update(DamageAction __instance, DamageInfo ___info)
+    {
+        if (((__instance.target).isDying || __instance.target.currentHealth <= 0) && !__instance.target.halfDead &&
+                !__instance.target.hasPower("Minion")) {
+            Object fatalSource = DamageModifierManager.BoundDamageInfoFields.instigatingObject.get(___info);
+            if (fatalSource instanceof AbstractCard)
+                FatalStrikesQuest.FATAL_CARD.trigger((AbstractCard) fatalSource);
+        }
+    }
+}
