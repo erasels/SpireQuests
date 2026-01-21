c912d1d81f7e4de9c886a58c52d083e5e1df01c8
modargo <modargo@gmail.com>
2026-01-21 15:29:39 -0500

    Treasure Map quest: fix the dig option not showing up when the player has the ruby key (due to an incorrectly targeted patch)

--- src/main/java/spireQuests/quests/jackrenoson/patches/TreasureMapXPatch.java
+++ src/main/java/spireQuests/quests/jackrenoson/patches/TreasureMapXPatch.java
@@ -1,25 +1,32 @@
 package spireQuests.quests.jackrenoson.patches;
 
-import com.evacipated.cardcrawl.modthespire.lib.SpireInsertPatch;
-import com.evacipated.cardcrawl.modthespire.lib.SpirePatch2;
+import com.evacipated.cardcrawl.modthespire.lib.*;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.rooms.CampfireUI;
 import com.megacrit.cardcrawl.ui.campfire.AbstractCampfireOption;
 import com.megacrit.cardcrawl.ui.campfire.DigOption;
+import javassist.CtBehavior;
 import spireQuests.quests.jackrenoson.TreasureMapQuest;
 import spireQuests.patches.ShowMarkedNodesOnMapPatch;
 
 import java.util.ArrayList;
 
 public class TreasureMapXPatch {
-
     @SpirePatch2(clz = CampfireUI.class, method = "initializeButtons")
     public static class PostButtonAddingCatcher {
-        @SpireInsertPatch(rloc = 25) // 117: boolean cannotProceed = true;
+        @SpireInsertPatch(locator = Locator.class)
         public static void postAddingButtons(CampfireUI __instance, ArrayList<AbstractCampfireOption> ___buttons) {
             if(ShowMarkedNodesOnMapPatch.ImageField.CheckMarks(AbstractDungeon.getCurrMapNode(), TreasureMapQuest.id, TreasureMapQuest.X))
                 ___buttons.add(new DigOption());
         }
     }
 
+    public static class Locator extends SpireInsertLocator {
+        @Override
+        public int[] Locate(CtBehavior ctBehavior) throws Exception {
+            Matcher finalMatcher = new Matcher.FieldAccessMatcher(AbstractPlayer.class, "relics");
+            return LineFinder.findInOrder(ctBehavior, finalMatcher);
+        }
+    }
 }
